c1386933e7869beabf752f0c0f1b8d36f87e1e60
Hansel <141368939+hanselpetter@users.noreply.github.com>
2023-09-06 21:21:23 +0900

    Remove the black&white effect and added the saturation effect

--- grunge.py
+++ grunge.py
@@ -5,7 +5,21 @@ import numpy as np
 image = cv2.imread('image1.jpeg')
 
 # conversion code
-image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+# Convert the image from BGR to HSV color space
+image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+  
+# Adjust the hue, saturation, and value of the image
+# Adjusts the hue by multiplying it by 0.7
+image[:, :, 0] = image[:, :, 0] * 1
+# Adjusts the saturation by multiplying it by 1.5
+image[:, :, 1] = image[:, :, 1] * 0.5
+# Adjusts the value by multiplying it by 0.5
+image[:, :, 2] = image[:, :, 2] * 1
+  
+# Convert the image back to BGR color space
+image = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
 
 # define the alpha and beta
 alpha = 0.5 # Contrast control
@@ -16,7 +30,7 @@ image = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
 
 
 # Generate random Gaussian noise
-mean = 10
+mean = 0
 stddev = 20
 noise = np.zeros(image.shape, np.uint8)
 cv2.randn(noise, mean, stddev)
@@ -30,7 +44,7 @@ image = cv2.blur(image, (6, 3))
 
 
 # Convert the image back to the RGB color space
-image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+# image = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
 
 cv2.imshow("image", image)
 
