04a9b69ec35ce00d3e52bcd9f1f882260c0ed85f
Hansel <141368939+hanselpetter@users.noreply.github.com>
2023-09-08 05:24:30 +0900

    Add functionality using a grunge texture image as a mask

--- grunge_texture.py
+++ grunge_texture.py
@@ -1,21 +1,41 @@
 import cv2
 import numpy as np
 
-img1 = cv2.imread('image.jpeg')
-overlay_img1 = np.ones(img1.shape,np.uint8)*255
+# Load the grunge texture image
+texture = cv2.imread("texture.jpg")
 
-img2 = cv2.imread('image.jpeg')
-rows,cols,channels = img2.shape
-overlay_img1[550:rows, 0:cols ] = img2[550:rows, 0:cols+250 ]
+# Convert the image to grayscale
+texture_gray = cv2.cvtColor(texture, cv2.COLOR_BGR2GRAY)
 
-img2gray = cv2.cvtColor(overlay_img1,cv2.COLOR_BGR2GRAY)
-ret, mask = cv2.threshold(img2gray,220,55,cv2.THRESH_BINARY_INV)
-mask_inv = cv2.bitwise_not(mask)
-temp1 = cv2.bitwise_and(img1,img1,mask = mask_inv)
-temp2 = cv2.bitwise_and(overlay_img1,overlay_img1, mask = mask)
+# Create a mask from the grunge texture image
+mask = texture_gray > 128
+mask = mask.astype(np.uint8)  # Convert the mask to uint8 data type
 
-result = cv2.add(temp1,temp2)
-cv2.imwrite("grunge_image.jpeg", result)
-cv2.imshow("Result",result)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# Load the image to be grunged
+image = cv2.imread("image.jpg")
+
+image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+  
+# Adjust the hue, saturation, and value of the image
+# Adjusts the hue by multiplying it by 0.7
+image[:, :, 0] = image[:, :, 0] * 0.2
+# Adjusts the saturation by multiplying it by 1.5
+image[:, :, 1] = image[:, :, 1] * 0.1
+# Adjusts the value by multiplying it by 0.5
+image[:, :, 2] = image[:, :, 2] * 1
+  
+# Convert the image back to BGR color space
+image = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
+
+# define the alpha and beta
+alpha = 0.8 # Contrast control
+beta = 5 # Brightness control
+
+# call convertScaleAbs function
+image = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
+
+# Apply the mask to the image
+grunge_image = cv2.bitwise_and(image, image, mask=mask)
+
+# Save the grunge image
+cv2.imwrite("grunge_image.jpg", grunge_image)
